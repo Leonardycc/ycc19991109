2bdd89bb02be67ab8b4159b8b0bb8b1c6314483a
Maxwell <sunmaxwell@outlook.com>
2018-10-03 09:30:08 +0800

    notes for day4

--- Day4/video_read.py
+++ Day4/video_read.py
@@ -1,23 +1,18 @@
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 
 cap = cv2.VideoCapture(2)
 
 while(True):
-    
-   
-
-    red = (0, 0, 255) #8
     ret, frame = cap.read()
-    cv2.line(frame, (300, 0), (0, 300), red, 3)
-    line=cv2.line(frame,(0,0),(511,511),(0,255,0),50)
-    cv2.imshow('2frame',frame)
-    gray = cv2.cvtColor(Vshow,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("frame",gray)
-    
-   
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('frame',frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gray',gray)
+    image90 = np.rot90(gray)
+    cv2.imshow('gray-90',image90)
+    rgb90 = np.rot90(frame)
+    cv2.imshow('rgb-90',rgb90)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
         break
 
 cap.release()
